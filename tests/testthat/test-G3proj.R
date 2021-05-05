8c7d3c6e61a357f83fe923cc9dc43cd6f4244958
Chanhwa Lee <chanhwa@email.unc.edu>
2021-05-05 00:40:39 -0400

    test for BLRM Tuning, Model fitting, Prediction added

--- tests/testthat/test-G3proj.R
+++ tests/testthat/test-G3proj.R
@@ -32,3 +32,39 @@ test_that("SSVS Tuning gives correct result",{
   expect_equal(colMeans(test1$gamma.samples),c(0.622, 0.542, 0.102, 0.070))
 })
 
+test_that("BLRM fitting gives correct result",{
+  set.seed(1)
+  N  = 800
+  p  = 4
+  X  = matrix(data = rnorm(N*p), nrow=N, ncol=p)
+  beta_true = c(rep(1,p/2),rep(0,p/2))
+  eta = X %*% beta_true
+  pi = exp(eta) / (1 + exp(eta))
+  Y  = rbinom(N,1,pi)
+  propSD = rep(1,p)
+
+  ## fit model;
+  test1 <- G3proj::BLRM_fit_mwg(Y0 = Y, X0 = X, PriorVar = 1000, propSD0 = propSD,
+                                nMC = 500, nBI = 50, seed = 1)
+  test1$beta.act.rate
+
+  expect_equal(test1$beta.act.rate, c(0.1276, 0.1188, 0.1044, 0.1068))
+})
+
+test_that("BLRM Tuning gives correct result",{
+  set.seed(1)
+  N  = 800
+  p  = 4
+  X  = matrix(data = rnorm(N*p), nrow=N, ncol=p)
+  beta_true = c(rep(1,p/2),rep(0,p/2))
+  eta = X %*% beta_true
+  pi = exp(eta) / (1 + exp(eta))
+  Y  = rbinom(N,1,pi)
+
+  ## fit model;
+  test1 <- G3proj::BLRM_Tuning(Y0 = Y, X0 = X, PriorVar = 1000,
+                                nMC = 500, b = 50, seed=1)
+
+  expect_equal(colMeans(test1$prop.sd.batch),c(0.945, 0.945, 0.945, 0.945))
+})
+
